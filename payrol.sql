30578a522fce17132218e238af4f57e587ecab01
Yudi Setyawan <47718902+cahbagus07@users.noreply.github.com>
2021-01-10 21:43:52 +0700

    Create payrol.sql

---
+++ payrol.sql
@@ -0,0 +1,235 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 10, 2021 at 03:43 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.3.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `payrol`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `absen`
+--
+
+CREATE TABLE `absen` (
+  `id_absen` int(11) NOT NULL,
+  `nip` varchar(50) NOT NULL,
+  `waktu` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `absen`
+--
+
+INSERT INTO `absen` (`id_absen`, `nip`, `waktu`) VALUES
+(1, '123', '2020-11-18 20:50:32'),
+(2, '123', '2020-11-18 20:50:34'),
+(3, '123', '2020-11-21 18:32:32'),
+(4, '123', '2020-12-21 15:58:47'),
+(5, '123', '2020-12-21 15:59:18'),
+(6, '1141', '2020-12-21 16:00:11');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cuti`
+--
+
+CREATE TABLE `cuti` (
+  `id_cuti` int(11) NOT NULL,
+  `nip` varchar(50) NOT NULL,
+  `mulai` date NOT NULL,
+  `akhir` date NOT NULL,
+  `alasan` text NOT NULL,
+  `status` enum('diajukan','diterima','ditolak') NOT NULL,
+  `waktu_pengajuan` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `cuti`
+--
+
+INSERT INTO `cuti` (`id_cuti`, `nip`, `mulai`, `akhir`, `alasan`, `status`, `waktu_pengajuan`) VALUES
+(2, '123', '2020-12-17', '2020-12-18', 'wefwfw', 'diajukan', '2020-12-22 02:30:35');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `departemen`
+--
+
+CREATE TABLE `departemen` (
+  `departemen_id` int(11) NOT NULL,
+  `departemen` varchar(254) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `departemen`
+--
+
+INSERT INTO `departemen` (`departemen_id`, `departemen`) VALUES
+(1, 'Keuangan'),
+(2, 'Administrasi');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pegawai`
+--
+
+CREATE TABLE `pegawai` (
+  `nip` varchar(50) NOT NULL,
+  `jenis_kelamin` enum('L','P') NOT NULL,
+  `waktu_masuk` date NOT NULL,
+  `id_departemen` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pegawai`
+--
+
+INSERT INTO `pegawai` (`nip`, `jenis_kelamin`, `waktu_masuk`, `id_departemen`) VALUES
+('1141', 'P', '2020-12-01', 2),
+('123', 'L', '2020-12-01', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `nama` varchar(254) NOT NULL,
+  `email` varchar(254) NOT NULL,
+  `password` varchar(254) NOT NULL,
+  `level` enum('admin','pegawai') NOT NULL,
+  `nip` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `nama`, `email`, `password`, `level`, `nip`) VALUES
+(1, 'Administrator', 'admin@admin.com', '202cb962ac59075b964b07152d234b70', 'admin', NULL),
+(2, 'Yudi Setyawan', 'yudi@gmail.com', '202cb962ac59075b964b07152d234b70', 'pegawai', '123'),
+(5, 'Neprisa', 'neprisa@gmail.com', '202cb962ac59075b964b07152d234b70', 'pegawai', '1141');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `web`
+--
+
+CREATE TABLE `web` (
+  `id_web` int(11) NOT NULL,
+  `logo` varchar(254) NOT NULL,
+  `nama` varchar(254) NOT NULL,
+  `author` varchar(254) NOT NULL,
+  `alamat` text NOT NULL,
+  `nohp` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `web`
+--
+
+INSERT INTO `web` (`id_web`, `logo`, `nama`, `author`, `alamat`, `nohp`, `email`) VALUES
+(1, 'fingerprint.png', 'E - Absensi', 'Yudi Setyawan', 'Gg. Damai No. 2, Rajabasa raya, Rajabasa', '082382000703', 'yudi.setyawan07@gmail.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `absen`
+--
+ALTER TABLE `absen`
+  ADD PRIMARY KEY (`id_absen`);
+
+--
+-- Indexes for table `cuti`
+--
+ALTER TABLE `cuti`
+  ADD PRIMARY KEY (`id_cuti`);
+
+--
+-- Indexes for table `departemen`
+--
+ALTER TABLE `departemen`
+  ADD PRIMARY KEY (`departemen_id`);
+
+--
+-- Indexes for table `pegawai`
+--
+ALTER TABLE `pegawai`
+  ADD PRIMARY KEY (`nip`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `web`
+--
+ALTER TABLE `web`
+  ADD PRIMARY KEY (`id_web`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `absen`
+--
+ALTER TABLE `absen`
+  MODIFY `id_absen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `cuti`
+--
+ALTER TABLE `cuti`
+  MODIFY `id_cuti` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `departemen`
+--
+ALTER TABLE `departemen`
+  MODIFY `departemen_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `web`
+--
+ALTER TABLE `web`
+  MODIFY `id_web` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
